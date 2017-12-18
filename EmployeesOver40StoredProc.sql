cd09f1d38ecba8078c1fabb42d78c45f90b135cb
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2017-12-18 12:40:02 -0500

    Update Employees over 40 SP

--- EmployeesOver40StoredProc.sql
+++ EmployeesOver40StoredProc.sql
@@ -14,11 +14,11 @@ BEGIN
 	SELECT p.first_name, p.last_name, e.employee_id, et.employee_type_desc, prt.person_type_desc, 
            pt.pay_type_desc, pay.hourly_rate, pp.hours_worked
 	FROM   employee e
-		   INNER JOIN employee_type et ON e.employee_type_id = et.employee_type_id 
-		   INNER JOIN pay_period pp ON e.employee_id = pp.employee_id
-		   INNER JOIN pay ON e.pay_id = pay.pay_id 
-		   INNER JOIN pay_type pt ON pay.pay_type_id = @pay_type_id 
-		   INNER JOIN person p ON e.person_id = p.person_id 
-		   INNER JOIN person_type prt ON p.person_type_id = prt.person_type_id
+	       INNER JOIN employee_type et ON e.employee_type_id = et.employee_type_id 
+	       INNER JOIN pay_period pp ON e.employee_id = pp.employee_id
+	       INNER JOIN pay ON e.pay_id = pay.pay_id 
+	       INNER JOIN pay_type pt ON pay.pay_type_id = @pay_type_id 
+	       INNER JOIN person p ON e.person_id = p.person_id 
+	       INNER JOIN person_type prt ON p.person_type_id = prt.person_type_id
 	WHERE  hours_worked > 40 AND pay_type_desc LIKE '%hourly%' AND PP.pp_end_date = @endPeriod
 END
